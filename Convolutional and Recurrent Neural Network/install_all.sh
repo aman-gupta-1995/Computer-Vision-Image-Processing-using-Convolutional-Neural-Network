92e17b51be53c8833ba4b1ab5b3b7349fb8cffb3
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-10-05 13:33:30 +0530

    Update install_all.sh

--- Convolutional and Recurrent Neural Network/install_all.sh
+++ Convolutional and Recurrent Neural Network/install_all.sh
@@ -1,6 +1,6 @@
  #!/bin/bash -e   
 #    
-#  Copyright (c) 2014, Facebook, Inc.         
+#  Copyright (c) 2014, Facebook, Inc.          
 #  All rights reserved.                    
 #                         
 #  This source code is licensed under the BSD-style license found in the                         
